3ba92e9619bd9e842b198d9a155864614573100f
seleniumpanda <seleniumpanda@gmail.com>
2022-10-28 20:12:07 -0400

    second css class

---
+++ src/test/java/com/cssSelector/sessions/SecondCSS_Session.java
@@ -0,0 +1,23 @@
+package com.cssSelector.sessions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SecondCSS_Session {
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.findElement(By.cssSelector("input#login1")).sendKeys("seleniumpanda@rediffmail.com");
+		driver.findElement(By.cssSelector("input#password")).sendKeys("Selenium@123");
+
+	}
+
+}
